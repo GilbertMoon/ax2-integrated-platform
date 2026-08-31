430ae1eae1bf9eeb0d4ba059937bf6f7d9d777d9
심우섭 <tladntjq1@gmail.com>
2026-08-31 18:46:59 +0900

    브랜치구조화수정

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ elif DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",  
         }
     }
 else:

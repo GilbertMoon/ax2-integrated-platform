6151ec827b81a8d97e2fcd57b58a22cffe6d3208
pang789789 <wlskrnrmf@gmail.com>
2026-09-01 15:09:23 +0900

    feat: expose Slack identities in admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from accounts.admin_permissions import OperationsReadOnlyAdminMixin
-from notifications.models import Notification
+from notifications.models import Notification, SlackIdentity
 
 
 @admin.register(Notification)
@@ -10,3 +10,18 @@ class NotificationAdmin(OperationsReadOnlyAdminMixin, admin.ModelAdmin):
     list_filter = ("category",)
     search_fields = ("title", "message", "recipient__email")
     readonly_fields = ("created_at",)
+
+
+@admin.register(SlackIdentity)
+class SlackIdentityAdmin(OperationsReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = (
+        "slack_display_name",
+        "slack_email",
+        "user",
+        "slack_user_id",
+        "is_active",
+        "synced_at",
+    )
+    list_filter = ("is_active",)
+    search_fields = ("slack_display_name", "slack_email", "slack_user_id", "user__email")
+    readonly_fields = ("synced_at",)
